4ca80b5478d9854abc62d08c5c44f543e60348a7
lebonq <lebonquentin@live.fr>
2023-03-20 11:26:55 +0100

    Change value of shadow

--- src/shaders/pbr_directional_light.fs.glsl
+++ src/shaders/pbr_directional_light.fs.glsl
@@ -136,7 +136,7 @@ void main()
       {
         float closestDepth = texture(uDirLightShadowMap, lightCoords.xy + vec2(x, y) * pixelSize).r;
         if (currentDepth > closestDepth + bias)
-        shadow += 0.91f;
+        shadow += 0.79f; //We dont put 1 to avoid the shadow to be too dark
       }
     }
     // Get average shadow
